e852f63957df83b99ed92e67efd884225ad01270
keenanlk <keenanlkaufman@gmail.com>
2019-05-21 23:36:39 -0500

    first

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCbUr6ntQlt0wazgSO5-0NXDtTB7UtDgOg",
+    authDomain: "harmoney-eade7.firebaseapp.com",
+    databaseURL: "https://harmoney-eade7.firebaseio.com",
+    projectId: "harmoney-eade7",
+    storageBucket: "harmoney-eade7.appspot.com",
+    messagingSenderId: "693584011870",
+    appId: "1:693584011870:web:4ce177fcdf4f0aa4"
+  }
 };
